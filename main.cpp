b49571b38529871778df9fbc807509f8e7b69338
TadejSevsek <tadej.sevsek123@gmail.com>
2025-04-22 12:09:41 +0200

    SCRUM-9 implementinng reading files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+
+vector<unsigned char> readFile(const string& filename) {
+    ifstream file(filename, ios::binary);
+    return vector<unsigned char>((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+}
+
+
+
