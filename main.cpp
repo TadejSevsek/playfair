733cdb50d03df9beb52b12fa4f44e735192ab85f
TadejSevsek <tadej.sevsek123@gmail.com>
2025-04-22 12:31:49 +0200

    SCRUM-4 skeleton of the function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <vector>
 #include <fstream>
-
+#include <unordered_set>
 using namespace std;
 
 
+
+vector<unsigned char> generateTable(const string& key) {
+    unordered_set<unsigned char> used;
+    vector<unsigned char> table;
+    return table;
+}
 vector<unsigned char> readFile(const string& filename) {
     ifstream file(filename, ios::binary);
     return vector<unsigned char>((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
